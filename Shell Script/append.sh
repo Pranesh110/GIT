cfef38c8fa1ef9a54c481caf63a9d26d552d7e2e
Pranesh110 <mail2pranesh0@gmail.com>
2026-09-24 17:59:22 +0530

    test

---
+++ Shell Script/append.sh
@@ -0,0 +1,14 @@
+echo -e "ENter file name: \c"
+read file
+if [ -f $file ]
+then
+    if [ -w $file ]
+    then
+    echo "Type data and press ctrl+d to quit"
+    cat >>  $file
+    else
+    echo "FIle dont have permission or not a file"
+    fi
+else
+echo "FIle not exsist"
+fi
