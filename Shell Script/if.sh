33f5f0dc7b75aa2de342456d32a9a810670f85aa
Pranesh110 <mail2pranesh0@gmail.com>
2026-09-24 13:50:19 +0530

    test

---
+++ Shell Script/if.sh
@@ -0,0 +1,25 @@
+num=10
+if [ $num -eq 10 ]
+then
+echo "it is 10"
+fi
+
+test=abc
+if [ $test == dc ]
+then
+echo "it is abc"
+else
+echo "Not abc"
+fi
+
+num=10
+if (( $num <= 10 ))
+then
+echo "it is 10"
+fi
+
+arg=("$@")
+if (( $arg == 10 ))
+then
+echo "it is 10"
+fi
